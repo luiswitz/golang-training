76909122f4ddea140ef78ebe6b297ce8ccccb7f9
Luis Henrique Witz <luiswitz@gmail.com>
2017-04-05 01:06:36 -0300

    Working with variables

---
+++ variables/shorthand/02/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	b := "golang"
+	c := 4.17
+	d := true
+
+	// Prints the type of value of variables
+	fmt.Printf("%T \n", a)
+	fmt.Printf("%T \n", b)
+	fmt.Printf("%T \n", c)
+	fmt.Printf("%T \n", d)
+}
