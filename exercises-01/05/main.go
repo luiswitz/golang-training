fdf414287248e7d60947a29b2c0302eb8aaa6c43
Luis Henrique Witz <luiswitz@gmail.com>
2017-04-07 13:01:55 -0300

    Exercises

--- exercises-01/05/main.go
+++ exercises-01/05/main.go
@@ -4,13 +4,7 @@ import "fmt"
 
 func main() {
 	for i := 1; i < 100; i++ {
-		if i%3 == 0 && i%5 == 0 {
-			fmt.Println("FizzBuzz")
-		} else if i%3 == 0 {
-			fmt.Println("Fizz")
-		} else if i%5 == 0 {
-			fmt.Println("Buzz")
-		} else {
+		if i%2 == 0 {
 			fmt.Println(i)
 		}
 	}
