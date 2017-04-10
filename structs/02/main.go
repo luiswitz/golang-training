3d6114ff31ff34ff62b45033d1e1704ada9b01aa
Luis Henrique Witz <luiswitz@gmail.com>
2017-04-10 15:48:35 -0300

    Working with structs

---
+++ structs/02/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	First string
+	Last  string
+	Age   int
+}
+
+type DoubleZero struct {
+	Person
+	First         string
+	LicenseToKill bool
+}
+
+func main() {
+	p1 := DoubleZero{
+		Person: Person{
+			First: "James",
+			Last:  "Bond",
+			Age:   20,
+		},
+		First:         "Double Zero Seven",
+		LicenseToKill: true,
+	}
+
+	p2 := DoubleZero{
+		Person: Person{
+			First: "Miss",
+			Last:  "MoneyPenny",
+			Age:   19,
+		},
+		First:         "If looks could kill",
+		LicenseToKill: true,
+	}
+
+	fmt.Println(p1.First, p1.Person.First)
+	fmt.Println(p2.First, p2.Person.First)
+}
